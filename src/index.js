682477cf34983cfe49f8c708afe831076af52b20
Kate <Riba0017@yandex.ru>
2020-09-30 21:30:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if(array == undefined || array.length == 0) return 0;
+    let min = array[0];
+    array.forEach(item => {
+        if(item < min) min = item;
+    });
+    return min;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if(array == undefined || array.length == 0) return 0;
+    let max = array[0];
+    array.forEach(item => {
+        if(item > max) max = item;
+    });
+    return max;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if(array == undefined || array.length == 0) return 0;
+    let sum = array.reduce((prev, cur) => {
+        return prev + cur
+    }, 0);
+    return sum / array.length;
+};
